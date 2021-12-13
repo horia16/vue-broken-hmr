34e203937825e100504d1944ca9631044de80d2d
Horia Rotaru <horia.rotaru1@gmail.com>
2021-12-13 18:16:40 +0000

    init

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,19 @@
+<template>
+  <h1>{{ msg }}</h1>
+  <ParentComponent>
+    <ChildComponent style="background-color: aqua" />
+  </ParentComponent>
+</template>
+
+<script setup lang="ts">
+import { ref } from "vue";
+import { ParentComponent, ChildComponent } from "vue-broken-hmr-lib";
+defineProps<{ msg: string }>();
+
+const count = ref(0);
+</script>
+<style>
+.white {
+  color: white;
+}
+</style>
